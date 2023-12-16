b9c6cc3ac1c9acafad3955fb4f32e0626ac53053
Ahmed Sellami <152439986+Ahmed3845@users.noreply.github.com>
2023-12-16 21:11:06 +0100

    Create user_data.sh

---
+++ user_data.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+read -p "Enter Username : " -s -t 5 username
+infoline=$(cat "/etc/passwd" | grep -E "^$username")
+if [[ -n $infoline ]]; then
+	IFS=: read user pw uid gid name home shell <<< "$infoline"
+	echo $'\n'"user=$user"$'\n'"pw=$pw"$'\n'"uid=$uid"$'\n'"gid=$gid"$'\n'"name=$name"$'\n'"home=$home"$'\n'"shell=$shell"
+else
+	echo $'\n'"No such a user" >&2
+	exit 1
+fi
+exit
